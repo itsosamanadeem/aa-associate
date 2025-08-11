425cba788936f04282c4027a55c7f5384ccbc530
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-11 18:31:47 +0500

    Account Move Lines Wizard

--- account_move_inherit/models/account_move_lines.py
+++ account_move_inherit/models/account_move_lines.py
@@ -1,21 +1,21 @@
 # models/account_move_line.py
 from odoo import models, api
 
-class AccountMoveLine(models.Model):
-    _inherit = 'account.move.line'
+class AccountMove(models.Model):
+    _inherit = 'account.move'
 
     def action_open_attribute_wizard(self):
-        if self.product_id and self.product_id.product_tmpl_id.attribute_line_ids:
-            return {
-                'type': 'ir.actions.act_window',
-                'name': 'Configure Product',
-                'res_model': 'product.attribute.invoice.wizard',
-                'view_mode': 'form',
-                'target': 'new',
-                'context': {'default_product_id': self.product_id.id},
-
-                # 'context': {
-                #     'default_invoice_line_id': self.id,
-                #     'default_product_tmpl_id': self.product_id.product_tmpl_id.id,
-                # }
+        line = self.invoice_line_ids.filtered(lambda l: l.product_id and l.product_id.product_tmpl_id.attribute_line_ids)[:1]
+        if not line:
+            return
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Configure Product',
+            'res_model': 'product.attribute.invoice.wizard',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {
+                'default_product_id': line.product_id.id,
+                'default_invoice_line_id': line.id,
             }
+        }
