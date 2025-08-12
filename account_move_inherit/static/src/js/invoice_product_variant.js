489eb43cf2aa7fa35f0d20b253c60ac539e80d24
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-12 12:14:50 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/invoice_product_variant.js
+++ account_move_inherit/static/src/js/invoice_product_variant.js
@@ -2,12 +2,28 @@
 
 import { SaleOrderLineProductField } from "@sale/js/sale_product_field";
 import { registry } from "@web/core/registry";
+import {
+    ProductLabelSectionAndNoteField,
+    productLabelSectionAndNoteField,
+} from "@account/components/product_label_section_and_note_field/product_label_section_and_note_field";
 
 export class AccountMoveLineProductField extends SaleOrderLineProductField {
     static template = "account_move_inherit.InvoiceProductField";
+    static props = {
+        ...ProductLabelSectionAndNoteField.props,
+        readonlyField: { type: Boolean, optional: true },
+    };
     setup() {
         super.setup()
     }
 }
-
-registry.category("fields").add("invoice_product_many2one", AccountMoveLineProductField);
+export const accountMoveLineProductField = {
+    ...productLabelSectionAndNoteField,
+    component: AccountMoveLineProductField,
+    extractProps(fieldInfo, dynamicInfo) {
+        const props = productLabelSectionAndNoteField.extractProps(...arguments);
+        props.readonlyField = dynamicInfo.readonly;
+        return props;
+    },
+};
+registry.category("fields").add("invoice_product_many2one", accountMoveLineProductField);
