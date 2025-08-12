faa99179dbea709ab17753aa59c8301cf35e123c
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-12 17:58:01 +0500

    Account Move Lines Wizard

--- account_move_inherit/controller/wizard_controll.py
+++ account_move_inherit/controller/wizard_controll.py
@@ -1,12 +1,14 @@
 # controllers/wizard_controller.py
 from odoo import http
 from odoo.http import request
+from odoo.exceptions import UserError
 
 class WizardController(http.Controller):
 
     @http.route('/open_variant_price_wizard', type='json', auth='user', methods=['POST'], csrf=False)
     def open_variant_price_wizard(self, product_id):
         product = request.env['product.product'].browse(product_id)
+        raise UserError(product)
         if not product.exists():
             return False  # JS should handle this
 
