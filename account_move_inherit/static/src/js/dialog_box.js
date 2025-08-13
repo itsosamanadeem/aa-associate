466e957569e7590738c8a3098c69c37f9a7fb6a0
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-13 12:49:56 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/dialog_box.js
+++ account_move_inherit/static/src/js/dialog_box.js
@@ -2,24 +2,42 @@
 import { Component } from "@odoo/owl";
 import { Dialog } from "@web/core/dialog/dialog";
 import { useService } from "@web/core/utils/hooks";
+
 export class ProductVariantDialog extends Component {
     static template = "account_move_inherit.ProductVariantDialog";
     static components = { Dialog };
     static props = {
-        variants: { type: Array },  // required: list of variants
+        variants: { type: Array, required: true },
+        onSelect: { type: Function, optional: true }, // callback for selected variant
         close: Function
     };
+
     setup() {
-        // Variants are passed as props
         console.log("Variants in Dialog:", this.props.variants);
+
+        // Prepare variant display data
         this.variantList = this.props.variants.map(v => ({
             id: v.id,
             name: v.name,
             price: v.price,
-            image: v.product_image,
             imageUrl: `/web/image/product.product/${v.id}/image_256`
         }));
-        this.actionService = useService("action");
+
+        this.orm = useService("orm");
+        this.notification = useService("notification");
+    }
+
+    async selectVariant(variant) {
+        try {
+            if (this.props.onSelect) {
+                await this.props.onSelect(variant);
+            }
+            this.notification.add(`Variant "${variant.name}" selected`, { type: "success" });
+            this.close();
+        } catch (error) {
+            this.notification.add("Error selecting variant", { type: "danger" });
+            console.error(error);
+        }
     }
 
     close() {
