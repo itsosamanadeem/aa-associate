7d75a795263f9387a0663920df8e21e0b5a012ab
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-20 17:49:22 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/invoice_product_variant.js
+++ account_move_inherit/static/src/js/invoice_product_variant.js
@@ -21,7 +21,7 @@ export class AccountMoveLineProductField extends Many2OneField {
         this.actionService = useService("action");
         this.dialog = useService("dialog");
         this.orm = useService("orm");
-        console.log(this.props.record._parentRecord.data.state);
+        this.checkState = this.props.record._parentRecord.data.state
         
     }
 
