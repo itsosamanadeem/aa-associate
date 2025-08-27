5820f36913b7d077b93f2155c5d1acd2d86a76a7
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-27 17:28:49 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/application_number.js
+++ account_move_inherit/static/src/js/application_number.js
@@ -21,13 +21,15 @@ export class ApplicationNumberField extends Component {
     }
 
     getValue(){
+        console.log("Getting value for", this.props.record.data[this.props.name] || []);
+        
         return this.props.record.data[this.props.name];
     }
     onValueChange(variant_name, newValue) {
         this.state.values[variant_name] = newValue;
         console.log("Updated values:", this.state.values);
         this.props.record.update({
-            [this.props.name]: this.getValue() ? this.getValue() : JSON.stringify(this.state.values),
+            [this.props.name]: this.getValue() 
         })
     }
 }
