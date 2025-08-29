8c64cf186db9b0980e99cfc00298aaaf0a7a0fc6
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-29 17:46:29 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/active_fields_check_box.js
+++ account_move_inherit/static/src/js/active_fields_check_box.js
@@ -17,16 +17,31 @@ import {
     useSubEnv,
 } from "@odoo/owl";
 import { Many2XAutocomplete } from "@web/views/fields/relational_utils";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
 export class ActiveFields extends Component {
     static template = "account_move_inherit.ActiveFields"
-    static components = { Many2XAutocomplete };
+    static components = {
+        Many2XAutocomplete,
+        ...standardFieldProps,
+    };
     setup() {
         this.getDomain = () => {
             return [["partner_id", "=", this.props.record.data.partner_id]];
         };
         console.log('this.env', this.env.searchModel);
-        
+        this.onUpdateTrademark = async (value) => {
+            // 1. Update Odoo record value
+            await this.props.record.update(
+            {[this.props.name]: value}
+            );
+
+            // 2. Run custom logic
+            console.log("Trademark updated to:", value);
+
+            // e.g., call a method on the record, reload, etc.
+            // this.env.bus.trigger("some_event", { recordId: this.props.record.resId });
+        };
     }
 }
 export const active_fields = {
