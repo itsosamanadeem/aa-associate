3e8c2ce51cb3ab7d8011dbfe53aaae6379c37ac1
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-30 16:24:24 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/active_field_service.js
+++ account_move_inherit/static/src/js/active_field_service.js
@@ -15,6 +15,8 @@ export const activeFieldsService = {
                     state[lineId] = {};
                 }
                 state[lineId][fieldName] = value;
+
+                return state;
             },
         };
     },
