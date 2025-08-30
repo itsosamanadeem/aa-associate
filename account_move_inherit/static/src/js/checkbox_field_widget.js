b7fde1a667b87a9212643ced9cd633976e3e6055
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-30 18:26:25 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/checkbox_field_widget.js
+++ account_move_inherit/static/src/js/checkbox_field_widget.js
@@ -4,22 +4,13 @@ import { registry } from "@web/core/registry";
 import { ListRenderer } from "@web/views/list/list_renderer";
 import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
 import {
-    productLabelSectionAndNoteOne2Many,
-    ProductLabelSectionAndNoteOne2Many,
-    ProductLabelSectionAndNoteListRender,
-} from '@account/components/product_label_section_and_note_field/product_label_section_and_note_field';
-import {
-    SectionAndNoteListRenderer,
     sectionAndNoteFieldOne2Many,
 } from "@account/components/section_and_note_fields_backend/section_and_note_fields_backend";
 
-export class InvoiceLineListRendererWithCheckbox extends ProductLabelSectionAndNoteOne2Many {
-    setup() {
-        super.setup()
-        console.log('inherited');
-
-        
-    }
+/**
+ * Step 1: Custom renderer extending SectionAndNoteListRenderer
+ */
+export class InvoiceLineListRendererWithCheckbox extends ListRenderer {
     renderBodyCell({ column, record, isAnchor, rowIndex, colIndex }) {
         const td = super.renderBodyCell({ column, record, isAnchor, rowIndex, colIndex });
 
@@ -42,13 +33,22 @@ export class InvoiceLineListRendererWithCheckbox extends ProductLabelSectionAndN
         return td;
     }
 }
-export const invoiceLineListRendererWithCheckbox = {
+
+/**
+ * Step 2: Custom X2Many using our renderer
+ */
+export class InvoiceLineX2ManyWithCheckbox extends X2ManyField {
+    static components = {
+        ...X2ManyField.components,
+        ListRenderer: InvoiceLineListRendererWithCheckbox,
+    };
+}
+
+/**
+ * Step 3: Register widget
+ */
+registry.category("fields").add("invoiceLine_list_renderer_with_checkbox", {
     ...x2ManyField,
-    component: InvoiceLineListRendererWithCheckbox,
+    component: InvoiceLineX2ManyWithCheckbox,
     additionalClasses: sectionAndNoteFieldOne2Many.additionalClasses,
-};
-
-registry
-    .category("fields")
-    .add("invoiceLine_list_renderer_with_checkbox", invoiceLineListRendererWithCheckbox);
-
+});
