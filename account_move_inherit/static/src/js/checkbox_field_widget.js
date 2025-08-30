abbf81f54dfae448e9d9a83d44fa0abf7c048221
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-30 17:44:40 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/checkbox_field_widget.js
+++ account_move_inherit/static/src/js/checkbox_field_widget.js
@@ -1,36 +1,38 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
-import { standardFieldProps } from "@web/views/fields/standard_field_props";
 import { CheckBox } from "@web/core/checkbox/checkbox";
-import { Component } from "@odoo/owl";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
+import { Component, onWillStart } from "@odoo/owl";
 
 export class FieldWithCheckbox extends Component {
     static template = "account_move_inherit.FieldWithCheckbox";
     static props = {
         ...standardFieldProps,
+        InnerField: { type: Object },   // original field widget
     };
     static components = { CheckBox };
-
-    setup() {
-        console.log('field values', this.props.record.data[this.props.name]);
-        
-    }
 
     onToggle(ev) {
         const record = this.props.record;
         const fieldName = this.props.name;
         const checked = ev.target.checked;
 
-        const newFlags = Object.assign({}, record.data.extra_flags || {});
+        const newFlags = Object.assign({}, record.data.field_flags || {});
         newFlags[fieldName] = checked;
 
-        record.update({ extra_flags: newFlags });
+        record.update({ field_flags: newFlags });
     }
 }
 
-export const fieldWithCheckbox = {
+registry.category("fields").add("with_checkbox", {
     component: FieldWithCheckbox,
-};
-
-registry.category("fields").add("field_with_checkbox", fieldWithCheckbox);
+    extractProps: (fieldInfo, dynamicInfo) => {
+        // wrap original field widget
+        const original = registry.category("fields").get(fieldInfo.widget || fieldInfo.type);
+        return {
+            InnerField: original.component,
+            ...dynamicInfo,
+        };
+    },
+});
