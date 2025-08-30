fc5f3def3eabfe231c55df2065bb48efda501bf5
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-30 18:08:01 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/checkbox_field_widget.js
+++ account_move_inherit/static/src/js/checkbox_field_widget.js
@@ -2,35 +2,40 @@
 
 import { registry } from "@web/core/registry";
 import { ListRenderer } from "@web/views/list/list_renderer";
+import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
 
-export class InvoiceLineRendererWithCheckbox extends ListRenderer {
+export class InvoiceLineListRendererWithCheckbox extends ListRenderer {
     renderBodyCell({ column, record, isAnchor, rowIndex, colIndex }) {
         const td = super.renderBodyCell({ column, record, isAnchor, rowIndex, colIndex });
 
-        // only apply on invoice lines
-        if (record.model === "account.move.line") {
+        if (record.resModel === "account.move.line") {
             const fieldName = column.name;
 
-            // create checkbox
             const checkbox = document.createElement("input");
             checkbox.type = "checkbox";
-            checkbox.style.marginLeft = "5px";
+            checkbox.style.marginLeft = "4px";
             checkbox.checked = record.data.extra_flags?.[fieldName] || false;
 
             checkbox.addEventListener("change", () => {
                 const newFlags = Object.assign({}, record.data.extra_flags || {});
                 newFlags[fieldName] = checkbox.checked;
-                record.update({ extra_flags: newFlags });  // updates in DB
+                record.update({ extra_flags: newFlags });
             });
 
             td.appendChild(checkbox);
         }
-
         return td;
     }
 }
 
-registry.category("views").add("invoice_line_list_with_checkbox", {
-    ...registry.category("views").get("list"),
-    Renderer: InvoiceLineRendererWithCheckbox,
+export class InvoiceLineX2ManyWithCheckbox extends X2ManyField {
+    static components = {
+        ...X2ManyField.components,
+        ListRenderer: InvoiceLineListRendererWithCheckbox,
+    }
+}
+
+registry.category("fields").add("invoice_line_x2many_checkbox", {
+    ...x2ManyField,
+    component: InvoiceLineX2ManyWithCheckbox,
 });
