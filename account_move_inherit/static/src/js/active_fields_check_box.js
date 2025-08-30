f6d255be2bcb7ad3dbc4c676ffc596657b0b367a
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-30 16:00:13 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/active_fields_check_box.js
+++ account_move_inherit/static/src/js/active_fields_check_box.js
@@ -62,7 +62,7 @@ export class ActiveFields extends Component {
         
         this.activeFields.toggle(record.resId, fieldName, ev.target.checked);
 
-        console.log('record', record, 'fieldName', fieldName, 'toggle', ev.target.checked);
+        console.log('record', record, 'fieldName', fieldName, 'toggle', ev.target);
         
     }
 
