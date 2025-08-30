53e1ef476d41ffdf66f2bc6b1e92b2355bc460f3
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-30 18:19:46 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/checkbox_field_widget.js
+++ account_move_inherit/static/src/js/checkbox_field_widget.js
@@ -3,46 +3,31 @@
 import { registry } from "@web/core/registry";
 import { ListRenderer } from "@web/views/list/list_renderer";
 import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
+import {
+    productLabelSectionAndNoteOne2Many,
+    ProductLabelSectionAndNoteOne2Many,
+    ProductLabelSectionAndNoteListRender,
+} from '@account/components/product_label_section_and_note_field/product_label_section_and_note_field';
+import {
+    SectionAndNoteListRenderer,
+    sectionAndNoteFieldOne2Many,
+} from "@account/components/section_and_note_fields_backend/section_and_note_fields_backend";
 
-export class InvoiceLineListRendererWithCheckbox extends ListRenderer {
-    setup(){
+export class InvoiceLineListRendererWithCheckbox extends ProductLabelSectionAndNoteOne2Many {
+    setup() {
         super.setup()
 
         console.log('inherited');
-        
-    }
-    
-    renderBodyCell({ column, record, isAnchor, rowIndex, colIndex }) {
-        const td = super.renderBodyCell({ column, record, isAnchor, rowIndex, colIndex });
 
-        if (record.resModel === "account.move.line") {
-            const fieldName = column.name;
-
-            const checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            checkbox.style.marginLeft = "4px";
-            checkbox.checked = record.data.extra_flags?.[fieldName] || false;
-
-            checkbox.addEventListener("change", () => {
-                const newFlags = Object.assign({}, record.data.extra_flags || {});
-                newFlags[fieldName] = checkbox.checked;
-                record.update({ extra_flags: newFlags });
-            });
-
-            td.appendChild(checkbox);
-        }
-        return td;
     }
 }
-
-export class InvoiceLineX2ManyWithCheckbox extends X2ManyField {
-    static components = {
-        ...X2ManyField.components,
-        ListRenderer: InvoiceLineListRendererWithCheckbox,
-    }
-}
-
-registry.category("fields").add("invoice_line_x2many_checkbox", {
+export const invoiceLineListRendererWithCheckbox = {
     ...x2ManyField,
-    component: InvoiceLineX2ManyWithCheckbox,
-});
+    component: InvoiceLineListRendererWithCheckbox,
+    additionalClasses: sectionAndNoteFieldOne2Many.additionalClasses,
+};
+
+registry
+    .category("fields")
+    .add("invoiceLine_list_renderer_with_checkbox", invoiceLineListRendererWithCheckbox);
+
