1d5c1dc04951cfb0823d4622e7165e2ee085c29a
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-08-31 15:21:40 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/checkbox_field_widget.js
+++ account_move_inherit/static/src/js/checkbox_field_widget.js
@@ -3,15 +3,27 @@
 import { registry } from "@web/core/registry";
 import { ListRenderer } from "@web/views/list/list_renderer";
 import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
+import { CheckBox } from "@web/core/checkbox/checkbox";
+import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
 export class InvoiceLineListRendererWithFieldCheckbox extends ListRenderer {
-    static components = { ...ListRenderer.components };
+    static components = { ...ListRenderer.components,CheckBox };
     static recordRowTemplate = "account_move_inherit.ListRenderer.RecordRowWithCheckbox";
-
-    onFieldCheckboxToggle(record, ev) {
-        const checked = ev.target.checked;
-        console.log(`Row checkbox for record ${record.id}:`, checked);
+    setup(){
+        console.log('record', this.props);
+        
     }
+    onToggle(ev) {
+        const record = this.props.record;
+        const fieldName = this.props.name;
+        const checked = ev.target.checked;
+
+        const newFlags = Object.assign({}, record.data.extra_flags || {});
+        newFlags[fieldName] = checked;
+
+        record.update({ extra_flags: newFlags });
+    }
+
 }
 
 export class InvoiceLineOne2ManyWithFieldCheckbox extends X2ManyField {
