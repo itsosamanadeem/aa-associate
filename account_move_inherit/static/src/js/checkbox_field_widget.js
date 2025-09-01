a38c649c3a8c4d332cc0b787911dbeb895069010
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-09-01 11:13:47 +0500

    Account Move Lines Wizard

--- account_move_inherit/static/src/js/checkbox_field_widget.js
+++ account_move_inherit/static/src/js/checkbox_field_widget.js
@@ -4,28 +4,39 @@ import { registry } from "@web/core/registry";
 import { ListRenderer } from "@web/views/list/list_renderer";
 import { X2ManyField, x2ManyField } from "@web/views/fields/x2many/x2many_field";
 import { CheckBox } from "@web/core/checkbox/checkbox";
-import { standardFieldProps } from "@web/views/fields/standard_field_props";
 
 export class InvoiceLineListRendererWithFieldCheckbox extends ListRenderer {
     static components = { ...ListRenderer.components, CheckBox };
     static recordRowTemplate = "account_move_inherit.ListRenderer.RecordRowWithCheckbox";
-    static props = {
-        ...standardFieldProps,
-    }
     setup() {
         super.setup()
-        // console.log('record!!!!', this.props);
 
     }
     onFieldCheckboxToggle(record, fieldName, ev) {
         const checked = ev.target.checked;
-        console.log('rec', record, 'fieldName', fieldName, 'checked', checked);
+        const recId = record.resId || record.id;   // use real db ID if exists, else virtual ID
+        console.log('Toggle for', recId, fieldName, checked);
 
+        // Clone current flags
         const newFlags = Object.assign({}, record.data.extra_flags || {});
-        newFlags[fieldName] = checked;
+
+        // Ensure line entry exists
+        if (!newFlags[recId]) {
+            newFlags[recId] = [];
+        }
+
+        // Add or remove field from array
+        if (checked) {
+            if (!newFlags[recId].includes(fieldName)) {
+                newFlags[recId].push(fieldName);
+            }
+        } else {
+            newFlags[recId] = newFlags[recId].filter(f => f !== fieldName);
+        }
 
         record.update({ extra_flags: newFlags });
     }
+
 
 }
 
