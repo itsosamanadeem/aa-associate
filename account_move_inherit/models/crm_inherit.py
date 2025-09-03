cb88da4fde767f61bb1341bea27e861a291c6683
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-09-03 13:37:38 +0500

    CRM

--- account_move_inherit/models/crm_inherit.py
+++ account_move_inherit/models/crm_inherit.py
@@ -7,7 +7,7 @@ class ResPartnerTrademark(models.Model):
     _rec_name = "trademark_name"
     
     sequence = fields.Integer(default=10)
-    partner_name = fields.Char(string="Partner")
+    partner_id = fields.Many2one("res.partner", string="Partner", ondelete="cascade")
     trademark_name = fields.Char(string="Trademark Name")
     trademark_number = fields.Char(string="Trademark Number")
     name = fields.Char(string="Display Name")  
@@ -15,8 +15,8 @@ class ResPartnerTrademark(models.Model):
     @api.model
     def create(self, vals):
         """Auto-fill partner name if coming from Partner form context"""
-        if not vals.get("partner_name") and self.env.context.get("default_partner_id"):
-            vals["partner_name"] = self.env.context["default_partner_id"]
+        if not vals.get("partner_id") and self.env.context.get("default_partner_id"):
+            vals["partner_id"] = self.env.context["default_partner_id"]
         return super().create(vals)
 
 class ResPartner(models.Model):
