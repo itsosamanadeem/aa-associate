9b8ef49e91b3d238ff28d43455c5ad9793038656
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-09-03 14:04:46 +0500

    CRM

--- account_move_inherit/models/crm_trademark_history.py
+++ account_move_inherit/models/crm_trademark_history.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, _
+from odoo import models, fields, _, api
 from odoo.exceptions import AccessError
 class TrademarkHistory(models.Model):
     _name = "trademark.history"
