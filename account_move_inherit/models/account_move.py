4ba19a87054eace58f91a70f4102f226ba55dced
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-09-24 17:49:50 +0500

    Report

--- account_move_inherit/models/account_move.py
+++ account_move_inherit/models/account_move.py
@@ -1,5 +1,5 @@
 # models/account_move.py
-from odoo import models, api
+from odoo import models, api, _, fields
 from odoo.exceptions import UserError
 
 class AccountMove(models.Model):
@@ -21,51 +21,47 @@ class AccountMove(models.Model):
         'line_ids.full_reconcile_id',
         'state')
     def _compute_amount(self):
-        """ Compute the amounts of the move based on its lines. """
-        values = super()._compute_amount()
-        if values.get('amount_untaxed') or values.get('amount_tax'):
-            # If the amounts are not computed, we raise an error.
-            # This is to ensure that the amounts are always computed correctly.
-            raise UserError(f"Computed values: {values}")
-        # for move in self:
-        #     total_untaxed, total_untaxed_currency = 0.0, 0.0
-        #     total_tax, total_tax_currency = 0.0, 0.0
-        #     total_residual, total_residual_currency = 0.0, 0.0
-        #     total, total_currency = 0.0, 0.0
+        for move in self:
+            total_untaxed, total_untaxed_currency = 0.0, 0.0
+            total_tax, total_tax_currency = 0.0, 0.0
+            total_residual, total_residual_currency = 0.0, 0.0
+            total, total_currency = 0.0, 0.0
 
-        #     for line in move.line_ids:
-        #         if move.is_invoice(True):
-        #             # === Invoices ===
-        #             if line.display_type == 'tax' or (line.display_type == 'rounding' and line.tax_repartition_line_id):
-        #                 # Tax amount.
-        #                 total_tax += line.balance
-        #                 total_tax_currency += line.amount_currency
-        #                 total += line.balance
-        #                 total_currency += line.amount_currency
-        #             elif line.display_type in ('product', 'rounding'):
-        #                 # Untaxed amount.
-        #                 total_untaxed += line.balance
-        #                 total_untaxed_currency += line.amount_currency
-        #                 total += line.balance
-        #                 total_currency += line.amount_currency
-        #             elif line.display_type == 'payment_term':
-        #                 # Residual amount.
-        #                 total_residual += line.amount_residual
-        #                 total_residual_currency += line.amount_residual_currency
-        #         else:
-        #             # === Miscellaneous journal entry ===
-        #             if line.debit:
-        #                 total += line.balance
-        #                 total_currency += line.amount_currency
+            for line in move.line_ids:
+                if move.is_invoice(True):
+                    # === Invoices ===
+                    if line.display_type == 'tax' or (line.display_type == 'rounding' and line.tax_repartition_line_id):
+                        # Tax amount.
+                        total_tax += line.balance
+                        total_tax_currency += line.amount_currency
+                        total += line.balance
+                        total_currency += line.amount_currency
+                    elif line.display_type in ('product', 'rounding'):
+                        # Untaxed amount.
+                        total_untaxed += line.balance
+                        total_untaxed_currency += line.amount_currency
+                        total += line.balance
+                        total_currency += line.amount_currency
+                    elif line.display_type == 'payment_term':
+                        # Residual amount.
+                        total_residual += line.amount_residual
+                        total_residual_currency += line.amount_residual_currency
+                else:
+                    # === Miscellaneous journal entry ===
+                    if line.debit:
+                        total += line.balance
+                        total_currency += line.amount_currency
 
-        #     sign = move.direction_sign
-        #     move.amount_untaxed = sign * total_untaxed_currency
-        #     move.amount_tax = sign * total_tax_currency
-        #     move.amount_total = sign * total_currency
-        #     move.amount_residual = -sign * total_residual_currency
-        #     move.amount_untaxed_signed = -total_untaxed
-        #     move.amount_untaxed_in_currency_signed = -total_untaxed_currency
-        #     move.amount_tax_signed = -total_tax
-        #     move.amount_total_signed = abs(total) if move.move_type == 'entry' else -total
-        #     move.amount_residual_signed = total_residual
-        #     move.amount_total_in_currency_signed = abs(move.amount_total) if move.move_type == 'entry' else -(sign * move.amount_total)
+            sign = move.direction_sign
+            move.amount_untaxed = sign * total_untaxed_currency
+            move.amount_tax = sign * total_tax_currency
+            move.amount_total = sign * total_currency
+            move.amount_residual = -sign * total_residual_currency
+            move.amount_untaxed_signed = -total_untaxed
+            move.amount_untaxed_in_currency_signed = -total_untaxed_currency
+            move.amount_tax_signed = -total_tax
+            move.amount_total_signed = abs(total) if move.move_type == 'entry' else -total
+            move.amount_residual_signed = total_residual
+            move.amount_total_in_currency_signed = abs(move.amount_total) if move.move_type == 'entry' else -(sign * move.amount_total)
+
+            raise UserError(_("Total Untaxed: %s, Total Tax: %s, Total: %s, Total Residual: %s") % (total_untaxed, total_tax, total, total_residual))
