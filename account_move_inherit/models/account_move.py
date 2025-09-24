8d3ee1a2e1ec0ebaa98ad7d1c0ef3409e59caf6d
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-09-24 19:30:28 +0500

    Tax

--- account_move_inherit/models/account_move.py
+++ account_move_inherit/models/account_move.py
@@ -93,13 +93,13 @@ class AccountMove(models.Model):
         for move in self:
             if move.is_invoice(include_receipts=True):
                 base_lines, _tax_lines = move._get_rounded_base_and_tax_lines()
+                raise UserError(f"Tax Totals: {base_lines}, {_tax_lines}")
                 move.tax_totals = self.env['account.tax']._get_tax_totals_summary(
                     base_lines=base_lines,
                     currency=move.currency_id,
                     company=move.company_id,
                     cash_rounding=move.invoice_cash_rounding_id,
                 )
-                raise UserError(f"Tax Totals: {move.tax_totals}")
                 move.tax_totals['display_in_company_currency'] = (
                     move.company_id.display_invoice_tax_company_currency
                     and move.company_currency_id != move.currency_id
