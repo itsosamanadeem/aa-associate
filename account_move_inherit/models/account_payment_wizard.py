95bd6f86f543718f38376d8f2a59aecba47ec9e3
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-09-26 23:09:50 +0500

    Payment

--- account_move_inherit/models/account_payment_wizard.py
+++ account_move_inherit/models/account_payment_wizard.py
@@ -8,12 +8,11 @@ class AccountReconcileWizard(models.TransientModel):
     check_number = fields.Char(string="Cheque Number")
     account_id = fields.Many2one('account.account', string='Account',check_company=True,help="The account used for this payment.", store=True)
     tax_id = fields.Many2one('account.tax', string='Tax',default=False,check_company=True, help="The tax used for this payment.", store=True)
-    # tax = fields.Float(string='Tax Rate (%)', help="The tax rate to be applied on the payment.", default=0.0)
     amount = fields.Monetary(currency_field='currency_id', store=True, readonly=True,compute='_compute_amount')
     taxed_amount = fields.Monetary(string='Taxed Amount', currency_field='currency_id', help="The amount of tax to be applied on the payment.", compute='_compute_taxed_amount', store=True, readonly=True)
-    untaxed_amount = fields.Monetary(string='Untaxed Amount', currency_field='currency_id', help="The amount without tax to be applied on the payment.",compute='_compute_amount', store=True)
+    untaxed_amount = fields.Monetary(string='Untaxed Amount', currency_field='currency_id', help="The amount without tax to be applied on the payment.",compute='_compute_amount', store=True, readonly=True)
 
-    @api.depends('untaxed_amount', 'tax_id')
+    @api.depends('tax_id')
     def _compute_taxed_amount(self):
         for record in self:
             if record.tax_id and record.untaxed_amount:
@@ -26,38 +25,20 @@ class AccountReconcileWizard(models.TransientModel):
     @api.depends(
         'can_edit_wizard', 'source_amount', 'source_amount_currency',
         'source_currency_id', 'company_id', 'currency_id',
-        'payment_date', 'installments_mode', 'tax_id'
+        'payment_date', 'installments_mode', 'taxed_amount',
     )
     def _compute_amount(self):
-        for wizard in self:
-            if not wizard.journal_id or not wizard.currency_id or not wizard.payment_date or wizard.custom_user_amount:
-                wizard.amount = wizard.amount or 0.0
-                wizard.untaxed_amount = wizard.untaxed_amount or 0.0
-            else:
-                total_amount_values = wizard._get_total_amounts_to_pay(wizard.batches) or {}
-                base_amount = total_amount_values.get('amount_by_default', 0.0)
-
-                # Untaxed amount = full amount from Odoo
-                wizard.untaxed_amount = total_amount_values.get('full_amount', base_amount)
-
-                # Apply tax if selected
-                if wizard.tax_id:
-                    tax_data = wizard.tax_id.compute_all(base_amount, wizard.currency_id, 1, partner=wizard.partner_id)
-                    tax_amount = sum(t['amount'] for t in tax_data['taxes'])
-                    wizard.amount = base_amount - tax_amount
+            for wizard in self:
+                if not wizard.journal_id or not wizard.currency_id or not wizard.payment_date or wizard.custom_user_amount:
+                    wizard.amount = wizard.amount
                 else:
-                    wizard.amount = base_amount
+                    total_amount_values = wizard._get_total_amounts_to_pay(wizard.batches)
+                    wizard.untaxed_amount = total_amount_values['amount_by_default']
+                    if wizard.tax_id and total_amount_values['amount_by_default']:
+                        wizard.amount = total_amount_values['amount_by_default'] - wizard.taxed_amount
+                    else:
+                        wizard.amount = total_amount_values['amount_by_default']
 
-
-    def _get_total_amounts_to_pay(self, batch_results):
-        rec = super()._get_total_amounts_to_pay(batch_results)
-        if not rec:
-            rec = {"amount_by_default": 0.0, "full_amount": 0.0}
-        rec.update({
-            "untaxed_amount": rec.get('full_amount', 0.0),
-            "taxed_amount": self.taxed_amount,
-        })
-        return rec
 
     def _create_payment_vals_from_wizard(self, batch_result):
         """Extend to add check fields into created payments"""
@@ -82,5 +63,3 @@ class AccountPayment(models.Model):
     taxed_amount = fields.Monetary(string='Taxed Amount', currency_field='currency_id', help="The amount of tax to be applied on the payment.",check_company=True,)
     untaxed_amount = fields.Monetary(string='Untaxed Amount', currency_field='currency_id', help="The amount without tax to be applied on the payment.",check_company=True,)
     
-
-    # tax = fields.Float(string='Tax Rate (%)', help="The tax rate to be applied on the payment.", default=0.0)
