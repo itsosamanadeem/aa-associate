7b18993a6be9b4081bc9e174797ef3e62a9c6845
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-09-29 13:48:54 +0500

    Small changes

--- account_move_inherit/models/account_payment_wizard.py
+++ account_move_inherit/models/account_payment_wizard.py
@@ -24,6 +24,19 @@ class AccountReconcileWizard(models.TransientModel):
     )
     account_id = fields.Many2one('account.account', string='Tax Account', required=False, check_company=True)
 
+    @api.depends('can_edit_wizard', 'amount', 'installments_mode')
+    def _compute_payment_difference(self):
+        for wizard in self:
+            if wizard.payment_date:
+                total_amount_values = wizard._get_total_amounts_to_pay(wizard.batches)
+                if wizard.installments_mode in ('overdue', 'next', 'before_date'):
+                    wizard.payment_difference = total_amount_values['amount_for_difference'] - wizard.amount - wizard.taxed_amount
+                elif wizard.installments_mode == 'full':
+                    wizard.payment_difference = total_amount_values['full_amount_for_difference'] - wizard.amount - wizard.taxed_amount
+                else:
+                    wizard.payment_difference = total_amount_values['amount_for_difference'] - wizard.amount - wizard.taxed_amount
+            else:
+                wizard.payment_difference = 0.0
     @api.depends(
         'can_edit_wizard', 'source_amount', 'source_amount_currency',
         'source_currency_id', 'company_id', 'currency_id',
