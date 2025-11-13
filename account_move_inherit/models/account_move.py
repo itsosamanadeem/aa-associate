9fd819d124fe1666666adefea715386a532dbe46
Osama Nadeem <muhammadnadeemqureshi77@gmail.com>
2025-11-13 15:32:56 +0500

    Total of fees

--- account_move_inherit/models/account_move.py
+++ account_move_inherit/models/account_move.py
@@ -9,10 +9,10 @@ class AccountMove(models.Model):
         string="Total Professional Fees",
         compute="_compute_professional_fees_total"
     )
-    total_official_fees = fields.Monetary(
+    total_offical_fees = fields.Monetary(
         currency_field="currency_id",
         string="Total Official Fees",
-        compute="_compute_official_fees_total"
+        compute="_compute_offical_fees_total"
     )
 
     @api.depends('invoice_line_ids.professional_fees',
@@ -27,14 +27,14 @@ class AccountMove(models.Model):
                     total += line.professional_fees
             rec.total_professional_fees = total
 
-    @api.depends('invoice_line_ids.official_fees',
+    @api.depends('invoice_line_ids.offical_fees',
                  'invoice_line_ids.lenght_of_classes')
-    def _compute_official_fees_total(self):
+    def _compute_offical_fees_total(self):
         for rec in self:
             total = 0
             for line in rec.invoice_line_ids:
                 if line.lenght_of_classes:
-                    total += line.official_fees * line.lenght_of_classes
+                    total += line.offical_fees * line.lenght_of_classes
                 else:
-                    total += line.official_fees
-            rec.total_official_fees = total
+                    total += line.offical_fees
+            rec.total_offical_fees = total
